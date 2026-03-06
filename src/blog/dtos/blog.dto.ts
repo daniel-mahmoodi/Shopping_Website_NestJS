cceb13aa2ec4ded0c60dfc3cc4d1a67bf3f2ba08
Daniel <mahmoodidanial2@gmail.com>
2026-03-06 19:02:28 +0100

    refactor: update BlogDto for validation

--- src/blog/dtos/blog.dto.ts
+++ src/blog/dtos/blog.dto.ts
@@ -1,4 +1,9 @@
+import { IsNotEmpty, IsString } from 'class-validator';
 export class BlogDto {
+  @IsString()
+  @IsNotEmpty()
   title: string;
+  @IsString()
+  @IsNotEmpty()
   content: string;
 }
