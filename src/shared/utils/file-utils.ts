9e7b4138d9fb59b707328ace2b4ed60847283d2a
Daniel <mahmoodidanial2@gmail.com>
2026-03-18 19:02:05 +0100

    Refactor: add unique naming style to uploaded images

--- src/shared/utils/file-utils.ts
+++ src/shared/utils/file-utils.ts
@@ -3,6 +3,7 @@ import * as mkdirp from 'mkdirp';
 
 export const SaveImage = async (file: Express.Multer.File) => {
   const destination = 'files/';
+  const fileName = new Date().toISOString() + '-' + file.originalname;
   mkdirp.sync(destination);
-  await sharp(file.buffer).toFile(destination + file.originalname);
+  await sharp(file.buffer).toFile(destination + fileName);
 };
